b5cf1a8072608ca122f0fe8b2c2f4b9c34706e1e
Evgeny <efonarev@mail.ru>
2023-07-28 10:57:07 +0300

    second commit

--- Solution.java
+++ Solution.java
@@ -1,12 +1,10 @@
 package com.javarush.task.task30.task3008;
 
 public class Solution {
-    private final Object lock1 = new Object();
     private final Object lock = new Object();
     private static int count=0;
 
     public synchronized void firstMethod(){
-        System.out.println("this1 = " + this);
         try{
             Thread.sleep(10);
         } catch(InterruptedException e){
@@ -18,37 +16,20 @@ public class Solution {
     }
 
     public void secondMethod() {
-        System.out.println("this2 = " + this);
-        synchronized(lock1){
-            try{
-                Thread.sleep(10);
-            } catch(InterruptedException e){
-                e.printStackTrace();
-            }
-            synchronized(lock){
-                doSomething();
-            }
-        }
-
-    }
-
-    public void thirdMethod() {
-        System.out.println("this2 = " + this);
         synchronized(lock){
             try{
                 Thread.sleep(10);
             } catch(InterruptedException e){
                 e.printStackTrace();
             }
-            synchronized(lock1){
+            synchronized(this){
                 doSomething();
             }
         }
-
     }
 
     private void doSomething() {
-        System.out.println(Thread.currentThread().getName() + " : this = " + this);
+        System.out.println(Thread.currentThread().getName());
     }
 
     public static void main(String[] args) {
@@ -56,13 +37,10 @@ public class Solution {
         System.out.println("Main started...");
         new Thread(new Runnable(){
             public void run(){
-                solution.thirdMethod();
-//                solution.secondMethod();
-            }
+                solution.firstMethod(); }
         }).start();
         new Thread(new Runnable(){
             public void run(){
- //               solution.firstMethod();
                solution.secondMethod();
             }
         }).start();
